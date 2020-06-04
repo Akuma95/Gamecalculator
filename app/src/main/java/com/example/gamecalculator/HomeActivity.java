71a4a0842f1b4221150610f4c793c8bb508369a5
Akuma95 <pio25995@gmail.com>
2020-06-04 12:01:36 +0200

    Homescreen hinzufügen

---
+++ app/src/main/java/com/example/gamecalculator/HomeActivity.java
@@ -0,0 +1,55 @@
+package com.example.gamecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.widget.ProgressBar;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000;
+
+    private ProgressBar bar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar2);
+
+        // timer for seekbar
+        final int oneMin = 1 * 4 * 1000; // 4 Sekunden in milli seconds
+
+        /** CountDownTimer starts with 4 seconds and every onTick is 1 second */
+        new CountDownTimer(oneMin, 1000) {
+            public void onTick(long millisUntilFinished) {
+
+                //forward progress
+                long finishedSeconds = oneMin - millisUntilFinished;
+                int total = (int) (((float)finishedSeconds / (float)oneMin) * 100.0);
+                progressBar.setProgress(total);
+
+//                //backward progress
+//                int total = (int) (((float) millisUntilFinished / (float) oneMin) * 100.0);
+//                progressBar.setProgress(total);
+
+            }
+
+            public void onFinish() {
+                // DO something when 1 minute is up
+            }
+        }.start();
+    }
+}
