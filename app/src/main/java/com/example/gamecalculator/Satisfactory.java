fc742bac38f9c1f5edb148405ff270de8105667f
Akuma95 <pio25995@gmail.com>
2020-06-08 09:34:38 +0200

    Fix - Verbindung zu Firebase Bug - Elemente in SatisfactoryItem sind nicht ansteuerbar

--- app/src/main/java/com/example/gamecalculator/Satisfactory.java
+++ app/src/main/java/com/example/gamecalculator/Satisfactory.java
@@ -13,10 +13,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import com.example.gamecalculator.ui.main.SectionsPagerAdapter;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Satisfactory extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+
+    private EditText category;
+    private EditText title;
+    private CheckBox isOre;
+    private Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +41,9 @@ public class Satisfactory extends AppCompatActivity {
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
+        submit = (Button)SatisfactoryItem.findViewById(R.id.successForm);
+        FirebaseApp.initializeApp(this);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,5 +52,28 @@ public class Satisfactory extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                category = findViewById(R.id.categorie);
+                title = findViewById(R.id.itemName);
+                isOre = findViewById(R.id.isOre);
+                writeItem();
+            }
+        });
+    }
+
+    private void writeItem() {
+        String categoryS = category.getText().toString();
+        String titleS = title.getText().toString();
+        String isOreS;
+        if (isOre.isChecked()) {
+            isOreS = "true";
+        } else {
+            isOreS = "false";
+        }
+
+        mDatabase.child("satisfactory").child(categoryS).setValue(titleS);
+        mDatabase.child("satisfactory").child(categoryS).setValue(isOreS);
     }
 }
